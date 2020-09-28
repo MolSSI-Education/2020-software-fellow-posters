1eae74b8f4a67e21018ee1bec39e5161c8c0f349
janash <janash@users.noreply.github.com>
2020-09-28 03:16:17 +0000

    jekyll build from Action 9f56c173a8975348dc7aaa05a554d886b4bff7d2

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,139 @@
+var store = [{
+        "title": "Using Machine Learning to Improve Coarse-Grained Simulations",
+        "excerpt":"Introduction Machine learning is a powerful and flexible toolset that can be used alongside molecular dynamics simulations to improve accuracy and learn underlying distributions and characteristics of a system of interest. Here we demonstrate the use of a new software method, HTF,1 that connects the machine learning library TensorFlow with...","categories": [],
+        "tags": [],
+        "url": "https://education.molssi.org/2020-software-fellow-posters/heta-gandhi/",
+        "teaser": null
+      },{
+        "title": "Latent space representation learning as an auxiliary task for training neural network potentials",
+        "excerpt":"Introduction Molecular properties can be obtained through a description of the electronic structure of any molecular system, which can be derived from high-level ab initio quantum mechanics (QM) by solving the Schrodinger equation. However, finding the exact solution to the many-body Schrodinger equation is NP-hard, or more specifically, QMA-hard (1),...","categories": [],
+        "tags": [],
+        "url": "https://education.molssi.org/2020-software-fellow-posters/farhad-ramezanghorbani/",
+        "teaser": null
+      },{
+        "title": "Relativistic Excited State Coupled-Cluster",
+        "excerpt":"Introduction The goal of this project is to implement and improve the preformance of excited state coupled cluster singles and doubles (CCSD) calculations in the Chronus Quantum software package,1 which can be used to generate absorption spectra in both the UV/Vis and X-ray range. This is acheived through two separate...","categories": [],
+        "tags": [],
+        "url": "https://education.molssi.org/2020-software-fellow-posters/lauren-koulias/",
+        "teaser": null
+      },{
+        "title": "Efficient implementation of ADC for ionization and electron attachment in molecules",
+        "excerpt":"Introduction Accurate computations of ionization potential (IP) and electron attachment (EA) are important for predicting properties of molecules and materials, such as redox potentials, band gaps, and photoelectron spectra. Development of reliable and efficient theoretical methods for computing IP and EA of molecules and materials is one of the current...","categories": [],
+        "tags": [],
+        "url": "https://education.molssi.org/2020-software-fellow-posters/samragni-banerjee/",
+        "teaser": null
+      },{
+        "title": "The ELSI Infrastructure for Scalable Electronic Structure Theory",
+        "excerpt":"The O(N3) Kohn-Sham Eigenproblem Kohn-Sham density-functional theory (KS-DFT) [1] Workhorse for molecular and materials simulations Large number of software implementations Running on world’s leading HPC resources Computational bottleneck of KS-DFT With local and semi-local exchange-correlation functionals, the solution of the eigenproblem \\(H C = S C \\Lambda\\) becomes the computational...","categories": [],
+        "tags": [],
+        "url": "https://education.molssi.org/2020-software-fellow-posters/victor-yu/",
+        "teaser": null
+      },{
+        "title": "QForte: A quantum computer simulator and algorithms library for molecular simulation",
+        "excerpt":"Background Classical simulation of molecules and materials via solution of the Schröodinger equation is largely prevented by exponentially scaling memory requirements.8 Quantum computational simulation offers a way to circumvent the storage bottleneck. An \\(n\\) qubit register is capable of storing a state in a Hilbert space of dimension \\(2^n\\), and...","categories": [],
+        "tags": [],
+        "url": "https://education.molssi.org/2020-software-fellow-posters/nick-stair/",
+        "teaser": null
+      },{
+        "title": "A High-Performance Hybrid Monte Carlo / Molecular Dynamics Framework For GROMACS",
+        "excerpt":"Introduction Classical molecular simulation is becoming increasingly capable of investigating important questions involving self-assembly, molecular association, conformational heterogeneity and phase behavior in biophysics, engineering and material science. However, as hardware and software have improved, it has become increasingly challenging to combine novel methodological advances with the most advanced high-performance simulation...","categories": [],
+        "tags": [],
+        "url": "https://education.molssi.org/2020-software-fellow-posters/pascal-merz/",
+        "teaser": null
+      },{
+        "title": "freud: Powerful, Efficient Trajectory Analysis in Scientific Python",
+        "excerpt":"Overview The freud Python library provides a simple, flexible, powerful set of tools for analyzing trajectories obtained from molecular dynamics or Monte Carlo simulations. High performance, parallelized C++ is used to compute standard tools such as radial distribution functions, correlation functions, order parameters, and clusters, as well as original analysis...","categories": [],
+        "tags": [],
+        "url": "https://education.molssi.org/2020-software-fellow-posters/bradley-dice/",
+        "teaser": null
+      },{
+        "title": "New Tools for Detecting MultirefPredict Character in Transition Metal Chemical Space",
+        "excerpt":"Introduction Open-shell transition metal complexes is essential for materials design and catalysis, but notoriously difficult to study accurately with electronic structure (ES) methods. Computationally predicted molecular properties can be strongly affected the choice of ES methods. Density functional theory (DFT) results can vary significantly with the choice of functionals and...","categories": [],
+        "tags": [],
+        "url": "https://education.molssi.org/2020-software-fellow-posters/fang-liu/",
+        "teaser": null
+      },{
+        "title": "Gradients and Geometry Optimization with Multireference Wavefunctions",
+        "excerpt":"Introduction In electronic structure, “active space methods” have been very successful for systems exhibiting strong electron correlation. However, their unfavorable combinatorial scaling has driven the search for new methods that better balance computational cost and accuracy facilitating their use with larger active spaces and larger systems. Modern methods, like density...","categories": [],
+        "tags": [],
+        "url": "https://education.molssi.org/2020-software-fellow-posters/james-smith/",
+        "teaser": null
+      },{
+        "title": "Reduced Density Matrix-Based Models for Strongly Correlated Electrons",
+        "excerpt":"Introduction The development of accurate and efficient models for the description of electron correlation effects is an active area of research in quantum chemistry and molecular physics. Although popular models such as Kohn-Sham density functional theory (DFT) [1a, b] have been successfully applied to a wide variety of molecular systems,...","categories": [],
+        "tags": [],
+        "url": "https://education.molssi.org/2020-software-fellow-posters/sina-mostafanejad/",
+        "teaser": null
+      },{
+        "title": "NeuralXC",
+        "excerpt":"A framework to train and deploy machine-learned density functionals Introduction Solving the many-body problem in quantum mechanics requires the use of approximate methods These methods come in several flavors that differ in accuracy and computational cost (one usually increases with the other). For small molecules, highly accurate, explicitly correlated methods...","categories": [],
+        "tags": [],
+        "url": "https://education.molssi.org/2020-software-fellow-posters/sebastian-dick/",
+        "teaser": null
+      },{
+        "title": "TCKDB - Theoretical Chemical Kinetics Database",
+        "excerpt":"Introduction One of the grand visions in the chemical kinetics community is that kinetic models will be generated automatically (see Reaction Mechanism Generator [1]), while calculating important and sensitive thermodynamic properties and reaction rate coefficients on-the-fly using quantum chemical electronic structure methods.[2] The TCKDB project seamlessly integrates into this vision...","categories": [],
+        "tags": [],
+        "url": "https://education.molssi.org/2020-software-fellow-posters/alon-grinberg-dana/",
+        "teaser": null
+      },{
+        "title": "OpenCAP: An open source package for studying resonances in molecules.",
+        "excerpt":"Motivation Electronic states which are metastable with respect to electron detachment, known as resonances, play central roles in electron-molecule scattering processes occurring in various contexts, including chemistry, physics, and biology. Despite the ubiquity of these processes in nature, the computational infrastructure available to the scientific community for studying metastable electronic...","categories": [],
+        "tags": [],
+        "url": "https://education.molssi.org/2020-software-fellow-posters/james-gayvert/",
+        "teaser": null
+      },{
+        "title": "Pattern recognition based framework to characterize order in materials - Focus on block copolymer self assembly",
+        "excerpt":"Introduction Block copolymers self-assemble to form a variety of different phases with highly regular patterns, depending on the microscopic ordering of molecules. Paramount to understanding and controlling this “order” is to have good “order parameters”, variables that can be used to track the changes occurring in the system as it...","categories": [],
+        "tags": [],
+        "url": "https://education.molssi.org/2020-software-fellow-posters/ankita-mukhtyar/",
+        "teaser": null
+      },{
+        "title": "Optimal allocation for Free Energy Calculations",
+        "excerpt":"Introduction Relative binding free energies A relative free energy calculation involves perturbing one molecule into another, and calculating the free energy difference of making that change. Relative binding free energies (RBFE) involve the perturbing between two molecules, both in an active site, and in bulk water to calculate the difference...","categories": [],
+        "tags": [],
+        "url": "https://education.molssi.org/2020-software-fellow-posters/hannah-brucemacdonald/",
+        "teaser": null
+      },{
+        "title": "DESPASITO, Quickly Fit MD-Simulation Parameters using Equations of State",
+        "excerpt":"Introduction First open-source application of its kind for Statistical Associating Fluid Theory (SAFT) equation of state (EOS). Despite the power of SAFT coarse graining, its use has been limited by lack of availability of a freely accessible application. Figure 1: DESPASITO: Determining Equilibrium State and Parametrization: Application for SAFT, Intended...","categories": [],
+        "tags": [],
+        "url": "https://education.molssi.org/2020-software-fellow-posters/jennifer-clark/",
+        "teaser": null
+      },{
+        "title": "Randomized Methods for Quantum Chemistry",
+        "excerpt":"Introduction Strong correlation among the electrons in many molecules and materials gives rise to unique and useful properties (e.g. catalytic behavior, superconductivity). Calculating properties of strongly correlated systems is expensive, requiring the solution of linear algebra problems involving large matrices. Many conventional approximations fail for strongly correlated problems. We are...","categories": [],
+        "tags": [],
+        "url": "https://education.molssi.org/2020-software-fellow-posters/samuel-greene/",
+        "teaser": null
+      },{
+        "title": "ATESA: an Automated Aimless Shooting Workflow",
+        "excerpt":"Introduction Transition path sampling methods are an indispensable tool for computational study of the dynamics of rare events in molecular simulations. However, access to advanced sampling techniques like transition path sampling is generally restricted to experts with the knowledge and resources to carefully set up, curate, and analyze the potentially...","categories": [],
+        "tags": [],
+        "url": "https://education.molssi.org/2020-software-fellow-posters/tucker-burgin/",
+        "teaser": null
+      },{
+        "title": "Tinker GPU: GPU-Accelerated MD Simulation Software for Advanced Force Fields and Enhanced Sampling",
+        "excerpt":"Introduction Advanced potential energy surfaces and models are being applied to an ever-expanding array of problems in structural biology, drug design and characterization of novel materials. These new potentials incorporate explicit polarization effects and are calibrated very closely against energy decompositions derived from electronic structure calculations. The Tinker molecular modeling...","categories": [],
+        "tags": [],
+        "url": "https://education.molssi.org/2020-software-fellow-posters/zhi-wang/",
+        "teaser": null
+      },{
+        "title": "Efficient Anharmonic Lattice Dynamics Calculations of Thermal Transport in Crystalline and Disordered Solids using kALDo",
+        "excerpt":"kALDo is a versatile and scalable open-source software to compute phonon transport in crystalline and amorphous solids. It features real space QHGK calculations and three different solvers of the linearized BTE: direct inversion, self-consistent cycle, and RTA. The algorithms are implemented using linear algebra operations on tensors, to take advantage...","categories": [],
+        "tags": [],
+        "url": "https://education.molssi.org/2020-software-fellow-posters/giuseppe-barbalinardo/",
+        "teaser": null
+      },{
+        "title": "Simple interoperability via a general representation of molecular wavefunctions",
+        "excerpt":"Background and Motivation The barrier to entry for preparing a new tool for use by the broader computational molecular science (CMS) community can be prohibitively high. Typically developers choose to restrict the implementation of their new tool into an established software package or construct an entirely new package from scratch....","categories": [],
+        "tags": [],
+        "url": "https://education.molssi.org/2020-software-fellow-posters/sebastian-lee/",
+        "teaser": null
+      },{
+        "title": "PDFT - An accessible density embedding code",
+        "excerpt":"Introduction Density embedding methods avoid the exponential scaling that hinders users from using accurate quantum chemistry in large and complex systems. Partition Density Functional Theory (PDFT)[1-4], makes use of functionals of non-interacting fragments, so that the energy of the entire system and any other electronic properties is given by functionals...","categories": [],
+        "tags": [],
+        "url": "https://education.molssi.org/2020-software-fellow-posters/vh-chavez/",
+        "teaser": null
+      }]
